7336e66bb4f83dc76be1774511648b75a66943bd
sidrobinson <sidneyjr.robinson@gmail.com>
2019-04-03 14:55:36 -0400

    Add files via upload

---
+++ game.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+    // let guess = point
+    let newGuess = Math.floor(guess);
+
+    let userWins = document.querySelector("#user-wins");
+    userWins.textContent = "I win";
+
+
+
+    if (newGuess += guess) {
+        yourReturn = ""
+    }
+    else {
+        if (newGuess += guess) {
+            yourReturn = ""
+        }
+        else {
+            yourReturn = ""
+        }
+    }
+})
